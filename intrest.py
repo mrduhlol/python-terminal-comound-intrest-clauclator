d9eb30279da73f66596a7d0b3c95e1130fc6d03d
Abhishek A <abhishek.a2534@gmail.com>
2026-05-13 23:42:54 +0530

    Final

--- intrest.py
+++ intrest.py
@@ -4,26 +4,26 @@ rate = 0
 time = 0
 
 while True:
-    principle = float(input("Enter the principle amount: $"))
+    principle = float(input("------------------------------\nEnter the principle amount: $"))
     if principle < 0:
         print("Principle amount can't be less than zero -_+")
     else:
         break
 
 while True:
-    rate = float(input("Enter the interest rate: "))
+    rate = float(input("------------------------------\nEnter the interest rate (in '%' ex: 0.5 for 5%): "))
     if rate < 0:
         print("Interest rate can't be less than zero -_+")
     else:
         break
 
 while True:
-    time = int(input("Enter the time period in terms of years: "))
+    time = int(input("------------------------------\nEnter the time period in terms of years: "))
     if time < 0:
         print("Time can't be less than zero -_+")
     else:
         break
 
 total = principle * pow((1 + rate/100), time)
-print(f"Balance after [\033[1m{time}\033[0m] year/s is: [\033[1m{total:.2f}\033[0m]")
+print(f"\n------------------------------\nBalance after [\033[1m{time}\033[0m] year/s is: [\033[1m{total:.2f}\033[0m]\n------------------------------\n\n")
 
